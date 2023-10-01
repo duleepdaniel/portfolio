05ac66c647b0bee49ab0aca80eb7d57bb2dfd16e
Duleepseneca <duleep.panthagani@senecaglobal.com>
2023-10-01 09:04:57 -0400

    CSS Changes 1

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ export default function About() {
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 sm:w-1/2 w-5/6">
-          <TagSphere />
+          <TagSphere radius="250"/>
         </div>
       </div>
     </section>
