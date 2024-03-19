102c965aeda658eff36a27c42f3abd3092c77772
Duleepseneca <duleep.panthagani@senecaglobal.com>
2024-03-19 00:11:04 -0400

    Chore: Change Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -45,17 +45,16 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=Ayodhya+Nagar+kakinada&zoom=12"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=Hanging+Moss+Cir,+Tampa,+FL+33613&zoom=12"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
+            <div className="lg:w-1/2 px-9">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 ADDRESS
               </h2>
               <p className="mt-1">
-                D.No: 3-18A-7/4 <br />
-                Ayodhya Nagar-4A<br />
-                Kakinada, AP, 533001
+                Hanging Moss CIR <br />
+                Tampa, FL, 33613
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -68,7 +67,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">9110317308</p>
+              <p className="leading-relaxed">+1(656)-212-7813</p>
             </div>
           </div>
         </div>
